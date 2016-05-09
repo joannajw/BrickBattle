8eec222fcc66487fa9a7b2fe947e95018c630b6c
Joanna Wang <joanna.j.wang@gmail.com>
2016-05-09 03:43:25 -0400

    Split colours

--- js/systemSettings.js
+++ js/systemSettings.js
@@ -327,6 +327,8 @@ var yOffset = roomHeight / 2 - (brickHeight * numRows) - spacing - 10;
 var bricks = [];
 var bricks_2 = [];
 
+var distBetween = 50;
+
 for (var j = 0; j < numRows; j++) {
     for (var i = 0; i < numCols; i++) {
         var idx = (j * numCols) + i;
@@ -335,33 +337,43 @@ for (var j = 0; j < numRows; j++) {
                             xMax: brickWidth * (i + 1) - (roomWidth / 2) - spacing,
                             yMin: j * brickHeight + spacing + yOffset,
                             yMax: (j + 1) * brickHeight - spacing + yOffset,
-                            zMin: -brickDepth / 2 + brickDepth / 2 + spacing,
-                            zMax: brickDepth / 2 + brickDepth / 2 + spacing,
+                            // zMin: distFromCenter,
+                            // zMax: distFromCenter + brickDepth,
+                            zMin: -brickDepth / 2 + brickDepth / 2 + distBetween,
+                            zMax: brickDepth / 2 + brickDepth / 2 + distBetween,
                             alive: true
                         };
+                        console.log((bricks[idx].box.zMin + bricks[idx].box.zMax )/ 2);
         bricks[idx].damping = 1;
         bricks_2[idx] = {};
         bricks_2[idx].box = { xMin: brickWidth * i - (roomWidth / 2) + spacing,
                             xMax: brickWidth * (i + 1) - (roomWidth / 2) - spacing,
                             yMin: j * brickHeight + spacing + yOffset,
                             yMax: (j + 1) * brickHeight - spacing + yOffset,
-                            zMin: -brickDepth / 2 - brickDepth / 2 - spacing,
-                            zMax: brickDepth / 2 - brickDepth / 2 - spacing,
+                            // zMin: -distFromCenter,
+                            // zMax: -distFromCenter - brickDepth / 2,
+                            zMin: -brickDepth / 2 - brickDepth / 2 - distBetween,
+                            zMax: brickDepth / 2 - brickDepth / 2 - distBetween,
                             alive: true
                         };
+                        console.log((bricks_2[idx].box.zMin + bricks[idx].box.zMax )/ 2);
         bricks_2[idx].damping = 1;    }
 }
+
 
 var platformWidth = brickWidth * 1.2;
 var platformHeight = 10;
 var platformDepth = brickDepth;
-var platformPosition = new THREE.Vector3(0, -roomHeight / 2 + 20, brickDepth / 2 + spacing);
-var platformPosition_2 = new THREE.Vector3(0, -roomHeight / 2 + 20, - brickDepth / 2 - spacing);
+var platformPosition = new THREE.Vector3(0, -roomHeight / 2, brickDepth / 2 + distBetween);
+var platformPosition_2 = new THREE.Vector3(0, -roomHeight / 2, - brickDepth / 2 - distBetween);
+// var platformPosition = new THREE.Vector3(0, -roomHeight / 2 + 20, brickDepth / 2 + spacing);
+// var platformPosition_2 = new THREE.Vector3(0, -roomHeight / 2 + 20, - brickDepth / 2 - spacing);
+console.log(platformPosition, platformPosition_2);
 var platformBox = { xMin: platformPosition.x - platformWidth / 2,
                     xMax: platformPosition.x + platformWidth / 2,
                     yMin: platformPosition.y - platformHeight / 2,
                     yMax: platformPosition.y + platformHeight / 2,
-                    zMin: platformPosition.z - platformDepth / 2,
+                    zMin: platformPosition.z - platformDepth / 2 - 10,
                     zMax: platformPosition.z + platformDepth / 2,
                     player: 1
                     };
@@ -370,7 +382,7 @@ var platformBox_2 = {   xMin: platformPosition_2.x - platformWidth / 2,
                         yMin: platformPosition_2.y - platformHeight / 2,
                         yMax: platformPosition_2.y + platformHeight / 2,
                         zMin: platformPosition_2.z - platformDepth / 2,
-                        zMax: platformPosition_2.z + platformDepth / 2,
+                        zMax: platformPosition_2.z + platformDepth / 2 + 10,
                         player: 2
                         };
 
@@ -430,47 +442,84 @@ SystemSettings.mySystem = {
     maxParticles :  2,
     particlesFreq : 100,
     createScene : function () {
-        var material_red     = new THREE.MeshPhongMaterial( {color: 0xFF0000, emissive: 0x222222, side: THREE.DoubleSide } );
-        var material_blue     = new THREE.MeshPhongMaterial( {color: 0x77CCFF, emissive: 0x222222, side: THREE.DoubleSide } );
-        var material_green     = new THREE.MeshPhongMaterial( {color: 0x00FF00, emissive: 0x222222, side: THREE.DoubleSide } );
-        var material_black     = new THREE.MeshPhongMaterial( {color: 0x000000, emissive: 0x222222, side: THREE.DoubleSide } );
-        var material_purple     = new THREE.MeshPhongMaterial( {color: 0xCC3399, emissive: 0x222222, side: THREE.DoubleSide } );
+
+        var material_player1_normal = new THREE.MeshPhongMaterial( {color: 0x00FF00, emissive: 0x222222, side: THREE.DoubleSide } );
+        var material_player1_light = new THREE.MeshPhongMaterial( {color: 0x00FF00, emissive: 0x222222, side: THREE.DoubleSide } );
+        var material_player1_dark = new THREE.MeshPhongMaterial( {color: 0x00FF00, emissive: 0x222222, side: THREE.DoubleSide } );
+
+        var material_player2_normal = new THREE.MeshPhongMaterial( {color: 0xCC3399, emissive: 0x222222, side: THREE.DoubleSide } );
+        var material_player2_light = new THREE.MeshPhongMaterial( {color: 0x00FF00, emissive: 0x222222, side: THREE.DoubleSide } );
+        var material_player2_dark = new THREE.MeshPhongMaterial( {color: 0x00FF00, emissive: 0x222222, side: THREE.DoubleSide } );
 
         // Ceiling
-        var plane_geo_top = new THREE.PlaneBufferGeometry( roomWidth, roomDepth, 1, 1 );
-        var plane_top     = new THREE.Mesh( plane_geo_top, material_blue );
+        var plane_geo_top = new THREE.PlaneBufferGeometry( roomWidth, roomDepth / 2, 1, 1 );
+        var plane_top     = new THREE.Mesh( plane_geo_top, material_player1_normal );
         plane_top.rotation.x = Math.PI / 2;
         plane_top.position.y = roomHeight + y_offset;
+        plane_top.position.z = roomDepth / 4;
         Scene.addObject( plane_top );
 
+        var plane_geo_top2 = new THREE.PlaneBufferGeometry( roomWidth, roomDepth / 2, 1, 1 );
+        var plane_top2     = new THREE.Mesh( plane_geo_top2, material_player2_normal );
+        plane_top2.rotation.x = Math.PI / 2;
+        plane_top2.position.y = roomHeight + y_offset;
+        plane_top2.position.z = -roomDepth / 4;
+        Scene.addObject( plane_top2 );
+
         // Floor (make this a sink plane?)
-        var plane_geo_bottom = new THREE.PlaneBufferGeometry( roomWidth, roomDepth, 1, 1 );
-        var plane_bottom     = new THREE.Mesh( plane_geo_bottom, material_black );
+        var plane_geo_bottom = new THREE.PlaneBufferGeometry( roomWidth, roomDepth / 2, 1, 1 );
+        var plane_bottom     = new THREE.Mesh( plane_geo_bottom, material_player1_normal );
         plane_bottom.rotation.x = -Math.PI / 2;
         plane_bottom.position.y = 0 + y_offset;
+        plane_bottom.position.z = roomDepth / 4;
         Scene.addObject( plane_bottom );
 
+        var plane_geo_bottom2 = new THREE.PlaneBufferGeometry( roomWidth, roomDepth / 2, 1, 1 );
+        var plane_bottom2     = new THREE.Mesh( plane_geo_bottom, material_player2_normal );
+        plane_bottom2.rotation.x = -Math.PI / 2;
+        plane_bottom2.position.y = 0 + y_offset;
+        plane_bottom2.position.z = -roomDepth / 4;
+        Scene.addObject( plane_bottom2 );
+
         // Left wall
-        var plane_geo_left = new THREE.PlaneBufferGeometry( roomDepth, roomHeight, 1, 1 );
-        var plane_left     = new THREE.Mesh( plane_geo_left, material_red );
+        var plane_geo_left = new THREE.PlaneBufferGeometry( roomDepth / 2, roomHeight, 1, 1 );
+        var plane_left     = new THREE.Mesh( plane_geo_left, material_player1_normal );
         plane_left.rotation.y = Math.PI / 2;
         plane_left.position.x = -roomWidth / 2;
         plane_left.position.y = roomHeight / 2 + y_offset;
+        plane_left.position.z = roomDepth / 4;
         Scene.addObject( plane_left );
 
+        var plane_geo_left2 = new THREE.PlaneBufferGeometry( roomDepth / 2, roomHeight, 1, 1 );
+        var plane_left2     = new THREE.Mesh( plane_geo_left2, material_player2_normal );
+        plane_left2.rotation.y = Math.PI / 2;
+        plane_left2.position.x = -roomWidth / 2;
+        plane_left2.position.y = roomHeight / 2 + y_offset;
+        plane_left2.position.z = -roomDepth / 4;
+        Scene.addObject( plane_left2 );
+
         // Right wall
-        var plane_geo_right = new THREE.PlaneBufferGeometry( roomDepth, roomHeight, 1, 1 );
-        var plane_right     = new THREE.Mesh( plane_geo_right, material_red );
+        var plane_geo_right = new THREE.PlaneBufferGeometry( roomDepth / 2, roomHeight, 1, 1 );
+        var plane_right     = new THREE.Mesh( plane_geo_right, material_player1_normal );
         plane_right.rotation.y = -Math.PI / 2;
         plane_right.position.x = roomWidth / 2;
         plane_right.position.y = roomHeight / 2 + y_offset;
+        plane_right.position.z = roomDepth / 4;
         Scene.addObject( plane_right );
+
+        var plane_geo_right2 = new THREE.PlaneBufferGeometry( roomDepth / 2, roomHeight, 1, 1 );
+        var plane_right2     = new THREE.Mesh( plane_geo_right2, material_player2_normal );
+        plane_right2.rotation.y = -Math.PI / 2;
+        plane_right2.position.x = roomWidth / 2;
+        plane_right2.position.y = roomHeight / 2 + y_offset;
+        plane_right2.position.z = -roomDepth / 4;
+        Scene.addObject( plane_right2 );
 
         // Add bricks
         for (var i = 0; i < bricks.length; i++) {
             var bound = bricks[i].box;
             var box_geo   = new THREE.BoxGeometry(bound.xMax - bound.xMin, bound.yMax - bound.yMin, bound.zMax - bound.zMin);
-            var box       = new THREE.Mesh( box_geo, material_green );
+            var box       = new THREE.Mesh( box_geo, material_player1_normal );
             box.position.set( (bound.xMin + bound.xMax) / 2, (bound.yMin + bound.yMax) / 2, (bound.zMin + bound.zMax) / 2 );
             Scene.addObject( box );
             bound.mesh = box;
@@ -480,7 +529,7 @@ SystemSettings.mySystem = {
         for (var i = 0; i < bricks_2.length; i++) {
             var bound = bricks_2[i].box;
             var box_geo   = new THREE.BoxGeometry(bound.xMax - bound.xMin, bound.yMax - bound.yMin, bound.zMax - bound.zMin);
-            var box       = new THREE.Mesh( box_geo, material_purple );
+            var box       = new THREE.Mesh( box_geo, material_player2_normal );
             box.position.set( (bound.xMin + bound.xMax) / 2, (bound.yMin + bound.yMax) / 2, (bound.zMin + bound.zMax) / 2 );
             Scene.addObject( box );
             bound.mesh = box;
@@ -488,23 +537,23 @@ SystemSettings.mySystem = {
 
         // Add player 1 platform
         var platform_geo   = new THREE.BoxGeometry(platformWidth, platformHeight, platformDepth);
-        var platform       = new THREE.Mesh( platform_geo, material_green );
+        var platform       = new THREE.Mesh( platform_geo, material_player1_normal );
         platform.position.set( platformPosition.x, platformPosition.y, platformPosition.z);
         Scene.addObject( platform );
         platformBox.mesh = platform;
 
         // Add player 2 platform
         var platform_geo_2   = new THREE.BoxGeometry(platformWidth, platformHeight, platformDepth);
-        var platform_2       = new THREE.Mesh( platform_geo_2, material_purple );
+        var platform_2       = new THREE.Mesh( platform_geo_2, material_player2_normal );
         platform_2.position.set( platformPosition_2.x, platformPosition_2.y, platformPosition_2.z);
         Scene.addObject( platform_2 );
         platformBox_2.mesh = platform_2;
 
-        // Add center line
-        var center_line_geo = new THREE.BoxGeometry(roomWidth, spacing, spacing);
-        var center_line     = new THREE.Mesh(center_line_geo, material_blue);
-        center_line.position.set(0, platformPosition.y, 0);
-        Scene.addObject(center_line);
+        // // Add center line
+        // var center_line_geo = new THREE.BoxGeometry(roomWidth, spacing, spacing);
+        // var center_line     = new THREE.Mesh(center_line_geo, material_blue);
+        // center_line.position.set(0, platformPosition.y, 0);
+        // Scene.addObject(center_line);
     },
 
 };
